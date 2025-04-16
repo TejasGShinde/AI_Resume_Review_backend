a7b1f246175eb7f4eb1d1e0d5621de178684f4db
TejasGShinde <ptejas387@gmail.com>
2025-04-16 22:24:56 +0530

    api added

--- controllers/gemini.js
+++ controllers/gemini.js
@@ -34,3 +34,32 @@ exports.geminiApi = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+exports.getResumeSummary = async (req, res) => {
+    try {
+        const { GoogleGenerativeAI } = await import("@google/generative-ai");
+        const genAI = new GoogleGenerativeAI("AIzaSyCPW9qdWakz-SBNvCcfCQFPFU2S8PTjAYM");
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+         console.log(__dirname)
+        // const filePath = path.join(__dirname, '..', '/controllers/resume.pdf');
+        // console.log(filePath)
+        // try {
+        //     await fs.access(filePath);
+        // } catch (err) {
+        //     return res.status(404).json({ error: "File not found" });
+        // }
+        const file = req.files[0];
+        const dataBuffer = await fs.readFile(file.path);
+        const pdfData = await pdfParse(dataBuffer);
+        const extractedText = pdfData.text;
+
+        const prompt = `Review the following resume data give point wise summary with ats score how to get notices and all :\n\n${extractedText}`;
+        const result = await model.generateContent(prompt);
+        const responseText = await result.response.text();
+
+        const cleanedResponse = responseText.replace(/\*/g, '').replace(/\n+/g, ' ').trim();
+        res.status(200).json({ message: cleanedResponse });
+    } catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).json({ error: error.message });
+    }
+};
--- routes/multiply.js
+++ routes/multiply.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const multer = require('multer')
 const { multiply } = require('../controllers/multiplication');
-const {geminiApi} = require('../controllers/gemini')
+const {geminiApi, getResumeSummary} = require('../controllers/gemini')
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' })
 router.get('/multiply',multiply);
 router.post('/gemini',upload.array('file', 12),geminiApi)
+router.post('/summary',upload.array('file', 12),getResumeSummary)
 module.exports = router;
